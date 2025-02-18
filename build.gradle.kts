f2a6efbe24bca9a1ecd19aecfea8ad4466aaa688
Hiroaki Akanuma <akanuma@unifa-e.com>
2025-02-19 08:55:12 +0900

    Add Call Logging plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.ktor.server.call.logging)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,11 +1,13 @@
 package com.example
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.calllogging.CallLogging
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(CallLogging)
     configureRouting()
 }
