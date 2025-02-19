6a0d6dcb8d13cd372cc3ace02217c68fa15b6e43
Hiroaki Akanuma <akanuma@unifa-e.com>
2025-02-19 09:11:01 +0900

    Add Content Negotiation plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ktor)
 }
 
@@ -22,8 +23,10 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
+    implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.server.call.logging)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -2,6 +2,8 @@ package com.example
 
 import io.ktor.server.application.*
 import io.ktor.server.plugins.calllogging.CallLogging
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -9,5 +11,8 @@ fun main(args: Array<String>) {
 
 fun Application.module() {
     install(CallLogging)
+    install(ContentNegotiation) {
+        json()
+    }
     configureRouting()
 }
