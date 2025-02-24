ed060c8541620d84194ef2a4af9febfa7d805180
Hiroaki Akanuma <akanuma@unifa-e.com>
2025-02-24 10:43:16 +0900

    Add CI build config.

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -36,3 +36,6 @@ tasks.withType<ShadowJar> {
 tasks.named<JavaExec>("run") {
     environment("JDBC_URL", "jdbc:mysql://localhost:3316/ktor_trial?allowPublicKeyRetrieval=true&useSSL=false")
 }
+tasks.withType<Test> {
+    environment("JDBC_URL", "jdbc:mysql://localhost:3317/ktor_trial?allowPublicKeyRetrieval=true&useSSL=false")
+}
--- infrastructure/src/test/kotlin/com/example/ApplicationTest.kt
+++ infrastructure/src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,18 +1,25 @@
 package com.example
 
+import com.typesafe.config.ConfigFactory
+import io.ktor.client.request.get
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.config.HoconApplicationConfig
 import io.ktor.server.testing.testApplication
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
-//        application {
-//            module()
-//        }
-//
-//        client.get("/").apply {
-//            assertEquals(HttpStatusCode.Companion.OK, status)
-//        }
+        environment {
+            config = HoconApplicationConfig(
+                ConfigFactory.systemEnvironment().withFallback(ConfigFactory.load())
+            )
+        }
+
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.Companion.OK, status)
+        }
     }
 
 }
